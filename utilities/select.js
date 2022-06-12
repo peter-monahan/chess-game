6dfc172db7411ee07ca565bb61aa294016d78975
peter <peter.b.monahan@gmail.com>
2022-06-12 07:14:54 -0500

    Added comments for lines 1-26

--- utilities/select.js
+++ utilities/select.js
@@ -1,31 +1,35 @@
-let selected = null;
+let selected = null; // Variable that will contain information about the selected piece. Null if no piece is selected.
 
 export function select(board) {
-  const gridDiv = document.querySelector('.grid-area');
+  const gridDiv = document.querySelector('.grid-area'); // Grabbing the chess board container.
 
 
-      gridDiv.addEventListener('click', e => {
-        console.log(board.grid);
-         const targ = e.target;
+      gridDiv.addEventListener('click', e => { // Adding an event listener for clicks on the chess board.
+         const targ = e.target; // Grabbing the 'target' of the click and saving it to a variable.
 
-        if(!selected) {
-           if (targ.dataset.piece.startsWith(board.turn[0])) {
-             targ.dataset.selected = 'true';
-             const piece = board.select(targ.id);
-             let validMoves = piece.getValidMoves();
-             validMoves = validMoves.map(el => el.join(','));
-             validMoves.forEach(id => {
+        if(!selected) { // If no piece is selected.
+
+           if (targ.dataset.piece.startsWith(board.turn[0])) { // If it is the clicked pieces turn.
+             targ.dataset.selected = 'true'; // Update the html elements data-selected to a string of 'true'.
+
+             const piece = board.select(targ.id); // Select the piece from the board object.
+
+             let validMoves = piece.getValidMoves(); // Get the valid moves of the piece.
+
+             validMoves = validMoves.map(el => el.join(',')); // Change each valid move into a string so as to target the grid id's.
+
+             validMoves.forEach(id => { // Update each valid html square's data-valid to a string of 'true'.
                const validSquare = document.getElementById(id);
                validSquare.dataset.valid = 'true';
              });
-             selected = {
+
+             selected = { // Save important information to the 'selected' variable.
                'element': targ,
                'pieceObj': piece,
                'pieceValidMoves': validMoves
               };
               console.log(selected);
            }
-
         } else if (targ.dataset.valid === 'true'){
           let newPiece = selected.element.dataset.piece;
           selected.pieceObj.move(targ.id.split(',').map(el => Number(el)));
